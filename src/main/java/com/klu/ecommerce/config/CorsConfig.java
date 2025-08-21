d472e27e1034d9bebc41b74bfdc9a75342466aa6
yaswanth5019k <yaswanthkalluri2005@gmail.com>
2025-08-21 23:11:58 +0530

    overall cors

--- src/main/java/com/klu/ecommerce/config/CorsConfig.java
+++ src/main/java/com/klu/ecommerce/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // your frontend URL
+                        .allowedOrigins("http://localhost:9191") // your frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
